0abcf4c4a5e42c036871851128a976da78b052f2
Garrett Rowell <Garrett2@cnwr.com>
2015-09-29 10:20:21 -0400

    add augeas to manage cacti.conf

--- manifests/config.pp
+++ manifests/config.pp
@@ -12,5 +12,15 @@ class cacti::config {
     group   => 'apache',
   }
 
+  augeas { 'cacti_perms':
+    incl => '/etc/httpd/conf.d/cacti.conf',
+    lens => 'Httpd.lns',
+    context => '/files/etc/httpd/conf.d/cacti.conf',
+    changes => [ 'set /files/etc/httpd/conf.d/cacti.conf/Directory[1]/IfModule[1]/directive/arg[1] all',
+      'set /files/etc/httpd/conf.d/cacti.conf/Directory[1]/IfModule[1]/directive/arg[2] granted',
+      'set /files/etc/httpd/conf.d/cacti.conf/Directory[1]/IfModule[2]/directive[3]/arg[2] all'
+    ],
+    require => Package['httpd']
+  }
 
 }
