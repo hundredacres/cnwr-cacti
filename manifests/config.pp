b9ffba9c5eb36e1023deaace55423f734993ef3d
Dan Beaman <dan.beaman@bbc.co.uk>
2016-06-28 10:52:34 +0100

    Refactor Augeas resource

--- manifests/config.pp
+++ manifests/config.pp
@@ -15,10 +15,13 @@ class cacti::config inherits cacti{
   augeas { 'cacti_perms':
     incl    => '/etc/httpd/conf.d/cacti.conf',
     lens    => 'Httpd.lns',
-    context => '/files/etc/httpd/conf.d/cacti.conf',
-    changes => [ 'set /files/etc/httpd/conf.d/cacti.conf/Directory[1]/IfModule[1]/directive/arg[1] all',
-      'set /files/etc/httpd/conf.d/cacti.conf/Directory[1]/IfModule[1]/directive/arg[2] granted',
-      'set /files/etc/httpd/conf.d/cacti.conf/Directory[1]/IfModule[2]/directive[3]/arg[2] all',
+    changes => [
+      'defnode req Directory[arg="/usr/share/cacti/"]/IfModule[arg="mod_authz_core.c"]/directive[.="Require"] "Require"',
+      'set $req/arg[1] "all"',
+      'set $req/arg[2] "granted"',
+
+      'defnode nomodauthzcore Directory[arg="/usr/share/cacti/"]/IfModule[arg="!mod_authz_core.c"] ""',
+      'set $nomodauthzcore/directive[.="Allow"]/arg[2] "all"',
     ],
   }
 
